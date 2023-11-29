b5a50bc9637f0e8bf40c055b793fc1fa75fde0f1
emanuele <emanuele971@icloud.com>
2023-11-29 09:43:24 +0100

    essaie1

---
+++ front-project/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './navbar'
+function App() {
+
+  return (
+<div>
+<Navbar />
+</div>
+  )
+}
+
+export default App
