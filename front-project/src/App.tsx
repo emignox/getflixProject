8898e6afa5b597ce756f6da5dd2386eac0d1d71f
Yannick Yanga <cleps2012@gmail.com>
2023-12-01 16:55:33 +0100

    Revert "get movies"
    This reverts commit 8c38a52bad01f5d854383e6b94872cc7e0407daf.

--- front-project/src/App.tsx
+++ front-project/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './navbar'
+import LoginForm from './login'
 
 function App() {
 
   return (
 <div>
   <Navbar />
+  <LoginForm />
 </div>
   )
 }
