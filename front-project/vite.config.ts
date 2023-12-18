0349770a8bbcab0b1a4b97aca2389737a7fa1e56
emanuele <emanuele971@icloud.com>
2023-12-18 16:27:59 +0100

    creation workflow

--- front-project/vite.config.ts
+++ front-project/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/getflixProject/',
-
+  base: '/getflixProject/front-project/',
   plugins: [react()],
 })
